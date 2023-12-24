d412a1ecd029db587d2b9087be6fe582871548b5
romeo kubeka <romeokubeka@gmail.com>
2023-12-24 08:43:03 +0000

    Set up Google Sheets authentication and access

--- expensetracker/run.py
+++ expensetracker/run.py
@@ -14,3 +14,10 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ] 
+
+# Load Google Sheets credentials from the JSON file
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/1TR5G47Vod-z4LYL8L5ptrYjAFKCOhmeneQodZjO19qE'
+SHEET = GSPREAD_CLIENT.open_by_url(SHEET_URL)
