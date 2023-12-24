a59c3d031e7a44986cc632fe4019c3249391538f
romeo kubeka <romeokubeka@gmail.com>
2023-12-24 08:40:25 +0000

    Configure gspread warnings suppression and set Google Sheets API scopes

--- expensetracker/run.py
+++ expensetracker/run.py
@@ -4,3 +4,13 @@ from google.oauth2.service_account import Credentials
 import warnings
 from tabulate import tabulate
 from datetime import datetime
+
+# Suppress the gspread warning
+warnings.filterwarnings("ignore", category=UserWarning, module="gspread")
+
+# Google Sheets scope
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+] 
